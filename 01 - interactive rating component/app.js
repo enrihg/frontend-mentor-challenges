a147a3cfbcd55740d0a72a0adeb573bd106082ae
enrihg <99612570+enrihg@users.noreply.github.com>
2023-05-29 20:27:53 -0300

    select a number and show it in the thank you page

--- 01 - interactive rating component/app.js
+++ 01 - interactive rating component/app.js
@@ -1,8 +1,18 @@
 const form = document.querySelector('.rating__card');
 const submitScreen = document.querySelector('.submit__screen');
+const ratingButtons = document.querySelectorAll('.button__rate');
+const rate = document.querySelector('#rate');
+
+for (let button of ratingButtons) {  
+    button.onclick = function () {
+        rate.innerHTML = button.innerHTML;
+    }
+}
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    submitScreen.classList.remove('disabled');
-    form.classList.add('disabled');
+    if( 1<rate.innerHTML || rate.innerHTML <6){
+        submitScreen.classList.remove('disabled');
+        form.classList.add('disabled');
+    } else alert('please select a number'); 
 });
