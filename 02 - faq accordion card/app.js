5cddd049e562482936bc03c409d5e0a0604f08cb
enrihg <99612570+enrihg@users.noreply.github.com>
2023-08-02 15:20:13 -0300

    mobile version done. Js refactoring

--- 02 - faq accordion card/app.js
+++ 02 - faq accordion card/app.js
@@ -1,9 +1,35 @@
 const container = document.querySelectorAll('.container');
-console.log(container);
 
 for (let i of container) {
     i.addEventListener('click', () => {
         console.log(i);
-        i.lastElementChild.classList.toggle("hidden");
+
+      
+        if (i.firstElementChild.classList.contains("bold")){
+            for (let i of container) {       
+                removeClass (i);
+            }
+        }
+        else {
+
+            for (let i of container) {
+                removeClass (i);
+            }
+            toggleClass (i);
+        }
     })
+}
+
+function removeClass (a) {
+    let i = a;
+    i.firstElementChild.classList.remove("bold");
+    i.firstElementChild.firstElementChild.classList.remove("rotate");
+    i.lastElementChild.classList.remove("show-answer");
+}
+
+function toggleClass (a) {
+    let i = a;
+    i.firstElementChild.classList.toggle("bold");
+    i.firstElementChild.firstElementChild.classList.toggle("rotate");
+    i.lastElementChild.classList.toggle("show-answer");
 }
